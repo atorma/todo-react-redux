cea7c435f4cc5662c7912310d92ddf0e24670332
Anssi Törmä <anssi.torma@iki.fi>
2019-10-11 08:41:28 +0300

    Implemented adding a new todo

--- src/todos/TodoList.tsx
+++ src/todos/TodoList.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
-import { Todo } from '../types';
+import React, { MouseEvent } from 'react';
+import { Todo, TodoAppState } from '../types';
 import TodoItem from './TodoItem';
+import { Button } from '@material-ui/core';
+import { AnyAction, Dispatch } from 'redux';
+import { actionCreators as todoActionCreators } from './todos-redux';
+import { connect } from 'react-redux';
 
-type TodoListProps = {
+type OwnProps = {
   todos: Todo[];
 };
+type StateProps = {};
+type DispatchProps = { saveTodo(todo: Todo): AnyAction };
+type Props = StateProps & DispatchProps & OwnProps;
 
-type TodoListState = {};
+type OwnState = {};
 
-class TodoList extends React.Component<TodoListProps, TodoListState> {
+class TodoList extends React.Component<Props, OwnState> {
+  constructor(props: Props) {
+    super(props);
+    this.handleAddNewClick = this.handleAddNewClick.bind(this);
+  }
+
   render() {
     return (
       <div>
@@ -17,8 +29,31 @@ class TodoList extends React.Component<TodoListProps, TodoListState> {
             <TodoItem todo={todo} />
           </div>
         ))}
+        <div>
+          <Button onClick={this.handleAddNewClick}>Add new</Button>
+        </div>
       </div>
     );
   }
+
+  handleAddNewClick(event: MouseEvent<HTMLButtonElement>): void {
+    event.preventDefault();
+    event.stopPropagation();
+    this.props.saveTodo({ id: '', title: 'New todo' });
+  }
 }
-export default TodoList;
+
+function mapStateToProps(state: TodoAppState, ownProps: OwnProps): StateProps {
+  return {};
+}
+
+function mapDispatchToProps(dispatch: Dispatch): DispatchProps {
+  return {
+    saveTodo: (todo: Todo) => dispatch(todoActionCreators.save(todo))
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList);
